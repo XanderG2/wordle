1821a805f9549dbad587c0541834516a4e99a8ef
Xander <XanderG2@users.noreply.github.com>
2026-02-06 17:10:40 +0000

    works now!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, time
 
 clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
 clearLine = lambda: print("\033[1A\033[2K\r", end="")
@@ -14,42 +14,56 @@ maxLetters = max(map(len, words))
 while True:
     try:
         letters = int(input("Please enter the amount of letters in the word you would like to guess:\n> "))
-        if 2 < letters <= maxLetters:
-            break
-        else:
+        if not 2 < letters <= maxLetters:
             print(f"Please enter a value between 3 and {maxLetters}.")
+            continue
+        break
     except ValueError:
         print("Please put a number.")
 
 validWords = [x.strip() for x in words if len(x.strip()) == letters]
 
 CORRECT = "\033[42m"       # Green background
-WRONG_PLACE = "\033[43m"   # Yello background
+WRONG_PLACE = "\033[43m"   # Yellow background
 WRONG = "\033[40m"         # Black background
 RESET = "\033[0m"          # Reset colour
 
 colors = input(CORRECT + "Can you see the background color? (y/n)" + RESET + "\n> ").lower() in ["y", "yes"]
+
+if not colors:
+    print("Havent added functionality for this yet lmao. Skill issue")
+    exit("Skill issue")
+
 word = random.choice(validWords)
 
 clear()
 
 print("--------- Python Wordle ---------")
 print(f"Selected letters: {letters}")
-print(word)
 print()
 
 while True:
     while True: # Main game loop
-        guess = input()
+        while True:
+            guess = input().strip()
+            if len(guess) != letters:
+                print("Please guess the correct amount of letters!")
+                time.sleep(2)
+                clearLine()
+                continue
+            break
         clearLine()
         truth = ""
         corrects = 0
-        for i, x in enumerate(guess):
+        lets = {w: a for w, a in zip(word, [word.count(x) for x in word])} # word: appearences // useful for yellow marking (if word has only 1 of a letter only
+        for i, x in enumerate(guess):                                                                                         # mark it once, etc.)
             if word[i] == x:
                 truth += CORRECT + x + RESET
                 corrects += 1
-            elif x in word:
+                lets[x] -= 1
+            elif x in word and lets[x] > 0:
                 truth += WRONG_PLACE + x + RESET
+                lets[x] -= 1
             else:
                 truth += WRONG + x + RESET
         print(truth)
